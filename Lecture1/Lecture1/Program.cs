fae7efb9785625a54be6f41f0135bf180bb9b991
jmplett <jmplett@me.com>
2022-01-11 16:09:16 -0800

    Creation of first c# file in vs code

---
+++ Lecture1/Lecture1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lecture1
+{
+    class Program
+    {
+        void Main()
+        {
+            string text = "Inigo Montoya";
+            Guid guid = Guid.NewGuid();
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Inigo Montoya"+ text);
+
+
+        }
+    }
+}
