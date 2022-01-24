a0ace3c2a6ac13e03e25bc39d54eb6fca0b3b8f7
Justin Plett <jmplett@me.com>
2022-01-24 13:13:37 -0800

    Creation of FileLogger & Update of FileLogger.test
    Tested the creation of a FIleLogger file

--- Logger.Tests/FileLoggerTests.cs
+++ Logger.Tests/FileLoggerTests.cs
@@ -5,6 +5,12 @@ namespace Logger.Tests
     [TestClass]
     public class FileLoggerTests
     {
-        
+
+        [TestMethod]
+        public void SimpleTest_CreateFileLogger_SuccessNotReturningNull()
+        {
+            FileLogger logger = new("FileLoggerTestFile.txt");
+            Assert.AreNotEqual(logger, null);
+        }
     }
 }
---
+++ Logger/FileLogger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Logger
+{
+	public class FileLogger:BaseLogger
+	{
+		public FileLogger(String filePath)
+		{
+            FilePath = filePath;
+		}
+
+        public string FilePath { get; }
+
+        public override void Log(LogLevel logLevel, string message)
+        {
+            using StreamWriter logFile = new(FilePath);
+            string currentTime = DateTime.Now.ToString();
+            logFile.WriteLine($"{currentTime} {logLevel}: {message}");
+        }
+    }
+}
+
