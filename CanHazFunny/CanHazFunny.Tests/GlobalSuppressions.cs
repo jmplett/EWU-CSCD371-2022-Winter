5104914cbef1b80103814ee449ff0f9e360abca9
Justin Plett <jmplett@me.com>
2022-02-03 18:19:04 -0800

    Suppression of needed warnings

---
+++ CanHazFunny/CanHazFunny.Tests/GlobalSuppressions.cs
@@ -0,0 +1,11 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "<Pending>", Scope = "member", Target = "~M:CanHazFunny.Tests.JesterTests.SimpleTest_WithNullJokeService_ThrowsException")]
+[assembly: SuppressMessage("Naming", "INTL0003:Methods PascalCase", Justification = "<Pending>", Scope = "member", Target = "~M:CanHazFunny.Tests.JesterTests.GetJoke_WithRemovalOfChuckNorris_Success")]
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "<Pending>", Scope = "member", Target = "~M:CanHazFunny.Tests.JesterTests.SimpleTest_CreateJester_Success")]
+[assembly: SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "<Pending>", Scope = "member", Target = "~M:CanHazFunny.Tests.IJokeServiceTest.GetJoke_NoParameters")]
