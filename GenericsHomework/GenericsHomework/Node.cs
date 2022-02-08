3d3d22fac05b5dad3cd0e8b79da2171281f11b6a
Zack Ward <zackward.student@gmail.com>
2022-02-08 15:20:51 -0800

    Start of Node class
    Initial idea

--- GenericsHomework/GenericsHomework/Node.cs
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -1,7 +1,23 @@
 ﻿namespace GenericsHomework
 {
-    public class Node
+    public class Node<T>
     {
+        private int[] size = { 0 };
+        private Node<T> next = null!;
+        private T? Value;
+        public int Size { get => size[0]; }
+
+        public Node<T> Next { 
+            get; 
+            private set; 
+        }
+
+        public Node (T value)
+            {
+            Value = value;
+            }
+
+            
 
     }
 }
