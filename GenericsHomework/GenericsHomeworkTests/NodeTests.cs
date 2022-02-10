d156fefcd26a28d03fc890a5c7a4fb633a2348f0
Justin Plett <jmplett@me.com>
2022-02-09 19:40:31 -0800

    Tests so far
    Will probably need modification later on

--- GenericsHomework/GenericsHomeworkTests/NodeTests.cs
+++ GenericsHomework/GenericsHomeworkTests/NodeTests.cs
@@ -1,37 +1,79 @@
+using GenericsHomework;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace GenericsHomeworkTests
 {
     [TestClass]
     public class NodeTests
     {
-        [Ignore]
+
         [TestMethod]
         public void SimpleTest_CreateNode_Success()
         {
-            NodeTests node = new();
-            Assert.AreEqual(node, 0);//need to a way to get size
+            Node<String> node = new("Hello");
+            Assert.AreEqual(node.Size, 1);
         }
 
-        [Ignore]
         [TestMethod]
-        public void SimpleTest_AppendNode_Success()
+        public void SimpleTest_CreateNewNode_ValueReturnsCorrectValue()
         {
+            Node<String> node = new("Hello");
+            Assert.AreEqual(node.Value, "Hello");
+        }
 
+        [TestMethod]
+        public void NodeListIsNull_AppendNode_Success()
+        {
+            Node<String> node = new("Node 1");
+            Assert.AreEqual(node.Size, 1);
+            node.Append("Pie");
+            Assert.AreEqual(node.Size, 2);
         }
 
         [Ignore]
         [TestMethod]
         public void SimpleTest_ClearNode_Success()
         {
-
+            Node<String> node = new("Node 1");
+            Assert.AreEqual(node.Size, 1);
+            node.Append("Pie");
+            Assert.AreEqual(node.Size, 2);
         }
 
         [Ignore]
         [TestMethod]
-        public void SimpleTest_NodeExists_Success()
+        public void SimpleTest_NodeExists_ReturnsTrue()
         {
+            Node<String> node = new("Node 1");
+            node.Append("Pie");
+            node.Append("Moon");
+            node.Append("Mines");
+            node.Append("Spys");
 
+            Assert.IsTrue(node.Exists("Moon")); //Only works for the final amended 
+        }
+
+        [TestMethod]
+        public void SimpleTest_NodeDoesntExists_ReturnsFalse()
+        {
+            Node<String> node = new("Node 1");
+            node.Append("Pie");
+            node.Append("Moon");
+            node.Append("Mines");
+            node.Append("Spys");
+
+            Assert.IsFalse(node.Exists("Justin")); //Only works for the final amended 
+        }
+
+
+        [TestMethod]
+        public void SimpleTest_CreateLinkedNodes_NextReturnsValueOfSecondNode()
+        {
+            Node<String> node = new("Node 1");
+            node.Append("Pie");
+
+            Assert.AreEqual("Pie", node.Next.Value);
         }
     }
 }
