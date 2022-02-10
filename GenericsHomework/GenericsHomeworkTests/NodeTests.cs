dd761da322e54d66c02329a0915d810587737d32
Justin Plett <jmplett@me.com>
2022-02-09 18:23:47 -0800

    Layout of needed tests

--- GenericsHomework/GenericsHomeworkTests/NodeTests.cs
+++ GenericsHomework/GenericsHomeworkTests/NodeTests.cs
@@ -5,9 +5,33 @@ namespace GenericsHomeworkTests
     [TestClass]
     public class NodeTests
     {
+        [Ignore]
         [TestMethod]
-        public void TestMethod1()
+        public void SimpleTest_CreateNode_Success()
         {
+            NodeTests node = new();
+            Assert.AreEqual(node, 0);//need to a way to get size
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void SimpleTest_AppendNode_Success()
+        {
+
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void SimpleTest_ClearNode_Success()
+        {
+
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void SimpleTest_NodeExists_Success()
+        {
+
         }
     }
 }
