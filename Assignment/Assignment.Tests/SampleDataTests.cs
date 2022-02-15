4eff2b487199df65b9f6fce7061741090d77d6dd
Justin Plett <jmplett@me.com>
2022-02-15 13:29:20 -0800

    Creation of SampleDataTests

---
+++ Assignment/Assignment.Tests/SampleDataTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Assignment.Tests
+{
+	[TestClass]
+	public class SampleDataTests
+	{
+		[Ignore]
+		[TestMethod]
+		public void CSVRows_Read_Success()
+		{
+	
+		}
+	}
+}
+
