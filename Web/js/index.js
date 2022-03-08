aa058ed16cb16d0ef24579ed926f87ddf6cad78f
Justin Plett <jmplett@me.com>
2022-03-07 21:13:48 -0800

    Joke Implementation

--- Web/js/index.js
+++ Web/js/index.js
@@ -1,3 +1,20 @@
 function showMenu() {
     document.querySelector(".dropItems").classList.toggle("show");
 }
+
+function getNewJoke() {
+    axios({
+            method: 'get',
+            url: 'https://v2.jokeapi.dev/joke/Programming'
+        })
+        .then(function(response) {
+            let joke = document.querySelector(".joke");
+            let punchline = document.querySelector(".punchline");
+
+            var jokeString = response.data.setup;
+            var deliveryString = response.data.delivery;
+
+            joke.innerText = jokeString;
+            punchline.innerText = deliveryString;
+        })
+}
