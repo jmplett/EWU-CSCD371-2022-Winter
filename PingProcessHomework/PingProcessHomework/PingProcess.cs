71978827e67ff83d3bd0825db7d95d5c3789ebe1
Zack Ward <zackward.student@gmail.com>
2022-03-21 13:10:16 -0700

    Starting
    Blocking out a few methods and getting things set up.

---
+++ PingProcessHomework/PingProcessHomework/PingProcess.cs
@@ -0,0 +1,14 @@
+﻿
+public class PingProcess
+{
+    public Task<PingResult> RunTaskAsync(string hostNameOrAddress)
+    {
+
+    }
+
+    async public Task<PingResult> RunAsync(string hostNameOrAddress)
+    {
+
+    }
+
+}
