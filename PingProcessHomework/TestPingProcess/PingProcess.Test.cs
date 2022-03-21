d495728c33b97ca6f79e7760c78a84e0848d7441
Zack Ward <zackward.student@gmail.com>
2022-03-21 13:14:44 -0700

    resynching
    Forgot to save something...

--- PingProcessHomework/TestPingProcess/PingProcess.Test.cs
+++ PingProcessHomework/TestPingProcess/PingProcess.Test.cs
@@ -11,13 +11,14 @@ namespace TestPingProcess
             //test method to test PingProcess.RunTaskAsync() using "localhost"
             //Do NOT use async/await in this implementation.
         }
-
+        [TestMethod]
         public void RunAsync_UsingTaskReturn_Success()
         {
             //test method to test PingProcess.RunAsync() using "localhost" without using async/await.
         }
 
         /* not yet implemented
+        [TestMethod]
         async public Task RunAsync_UsingTpl_Success()
         {
             // test method to test PingProcess.RunAsync() using "localhost" but this time DO using async/ await.
